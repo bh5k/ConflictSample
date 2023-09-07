3b76de9086d7772a6a20ef4265c8df54902db356
Bryan <bryankhansen@gmail.com>
2023-09-07 04:49:34 -0600

    Update ConflictSampleApp.java

--- src/main/java/org/example/ConflictSampleApp.java
+++ src/main/java/org/example/ConflictSampleApp.java
@@ -2,18 +2,12 @@ package org.example;
 
 public class ConflictSampleApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
 
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-
+        System.out.println("Not the changes we are looking for!");
+        System.out.println("Not the changes we are looking for!");
+        System.out.println("Not the changes we are looking for!");
+        System.out.println("Not the changes we are looking for!");
+        System.out.println("Not the changes we are looking for!");
+        System.out.println("Not the changes we are looking for!");
     }
 }
