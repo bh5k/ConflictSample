b7364cfa2489636d49b4fb133bac3ca779f91202
JoshuaDayan <144036077+JoshuaDayan@users.noreply.github.com>
2023-09-07 11:40:51 +0100

    Added written content

--- src/main/java/org/example/ConflictSampleApp.java
+++ src/main/java/org/example/ConflictSampleApp.java
@@ -3,5 +3,17 @@ package org.example;
 public class ConflictSampleApp {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
     }
 }
